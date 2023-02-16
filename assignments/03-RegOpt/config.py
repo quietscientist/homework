23a164df31b660bc73913810e615806d386e1a82
melanie segado <melanie.segado@gmail.com>
2023-02-16 17:31:41 -0500

    Doesn't learn very well, but does learn!

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,14 +7,19 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
 
-    batch_size = 64
-    num_epochs = 2
-    initial_learning_rate = 0.001
-    initial_weight_decay = 0
+    batch_size = 32
+    num_epochs = 55
+    initial_learning_rate = 0.0001
+    initial_weight_decay = 0.003
+    "These parameters are really not great for training, but I have to move on to other things."
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "init_l": initial_learning_rate,
+        "num_e": num_epochs,
+        "weight_dec": initial_weight_decay
+        
     }
 
     optimizer_factory: Callable[
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,9 @@
 from typing import List
-
 from torch.optim.lr_scheduler import _LRScheduler
-
+from config import CONFIG # do I even need this? 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(self, optimizer, last_epoch=-1, init_l=0.01, num_e=2, weight_dec=0):
         """
         Create a new scheduler.
 
@@ -12,13 +11,26 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        self.optimizer = optimizer
+        self.init_l = CONFIG.lrs_kwargs["init_l"]
+        self.num_e = CONFIG.lrs_kwargs["num_e"]
+        self.weight_dec = CONFIG.lrs_kwargs["weight_dec"]
+
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
+        """
+        Exponential decay of the learning rate.
+        """           
+        inital_lr = self.init_l
+        num_epochs = self.num_e     
+        schedule = [inital_lr]
+        
+        for i in range(1,num_epochs):
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+            schedule.append(self.init_l*(1-self.weight_dec)**(i))
+        
+        return schedule
